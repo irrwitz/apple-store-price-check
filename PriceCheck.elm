9b90f20e0c7818662df4a37611ed36fcc2751312
Joshy Cyriac <j.cyriac@gmail.com>
2015-07-20 23:02:27 +0200

    Begins to maka a http call to more that one item

--- PriceCheck.elm
+++ PriceCheck.elm
@@ -4,6 +4,17 @@ import Task exposing (Task, andThen)
 import Http exposing (..)
 import Json.Decode as Json exposing ((:=))
 
+appIds : List Int
+appIds = [ 881270303 -- xcom
+         , 911006986 -- banner saga
+         , 980307863 -- tales from deep space
+         , 978524071 -- grim fandango
+         , 680366065 -- device 6
+         , 895869909 -- the sailor's dream
+         , 555916407 --year walk
+         , 395680819 -- av player
+         ]
+
 
 main : Signal Element
 main =
@@ -25,10 +36,13 @@ port fetchItem : Task Http.Error ()
 port fetchItem =
   Http.get app itemUrl `andThen` sendToMailbox
 
+itemStoreUrl : Int -> String
+itemStoreUrl appId =
+  "https://itunes.apple.com/lookup?country=ch&id=" ++ appId.toString a
 
 itemUrl : String
 itemUrl =
-  "https://itunes.apple.com/lookup?id=881270303"
+  "https://itunes.apple.com/lookup?country=ch&id=881270303"
 
 
 app : Json.Decoder (List String)
